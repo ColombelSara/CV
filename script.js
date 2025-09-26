0ba67b8028c1f0937b53d181d3410295932da579
ColombelSara <sara.colombel@epitech.eu>
2025-09-26 11:58:15 +0200

    update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".mobile-menu");
+// const burger = document.querySelector(".burger");
+// const menu = document.querySelector(".mobile-menu");
 
-burger.addEventListener("click", () => {
-	menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-	menu.classList.toggle("open");
-});
+// burger.addEventListener("click", () => {
+// 	menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+// 	menu.classList.toggle("open");
+// });
 
-const sections = document.querySelector("#sections");
-const dots = document.querySelectorAll(".dot");
+// const sections = document.querySelector("#sections");
+// const dots = document.querySelectorAll(".dot");
 
-function updateDots() {
-	const index = Math.round(sections.scrollLeft / window.innerWidth);
-	dots.forEach((dot, i) => {
-		dot.classList.toggle("active", i === index);
-	});
-}
+// function updateDots() {
+// 	const index = Math.round(sections.scrollLeft / window.innerWidth);
+// 	dots.forEach((dot, i) => {
+// 		dot.classList.toggle("active", i === index);
+// 	});
+// }
 
-dots.forEach((dot) => {
-	dot.addEventListener("click", () => {
-		const index = dot.dataset.index;
-		sections.scrollTo({
-			left: window.innerWidth * index,
-			behavior: "smooth",
-		});
-	});
-});
+// dots.forEach((dot) => {
+// 	dot.addEventListener("click", () => {
+// 		const index = dot.dataset.index;
+// 		sections.scrollTo({
+// 			left: window.innerWidth * index,
+// 			behavior: "smooth",
+// 		});
+// 	});
+// });
 
-sections.addEventListener("scroll", updateDots);
+// sections.addEventListener("scroll", updateDots);
